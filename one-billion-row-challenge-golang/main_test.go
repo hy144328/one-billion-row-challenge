08875866081441f287566da82a4edd3bc591005a
Hans Yu <hans.yu@outlook.de>
2026-01-09 02:14:12 +0100

    Automate benchmarks.

--- one-billion-row-challenge-golang/main_test.go
+++ one-billion-row-challenge-golang/main_test.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"flag"
 	"os"
 	"testing"
 )
 
+var measurementsPath = flag.String("measurements", "measurements_7.txt", "Input file with measurements.")
+
 func BenchmarkRun(b *testing.B) {
 	for b.Loop() {
-		f, err := os.Open("measurements_7.txt")
+		f, err := os.Open(*measurementsPath)
 		if err != nil {
 			panic(err)
 		}
