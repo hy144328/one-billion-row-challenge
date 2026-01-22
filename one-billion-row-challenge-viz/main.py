aabe48b7f87385db913ed162e811c3e012d223ec
Hans Yu <hans.yu@outlook.de>
2026-01-23 00:22:20 +0100

    Two images.

--- one-billion-row-challenge-viz/main.py
+++ one-billion-row-challenge-viz/main.py
@@ -1,17 +1,64 @@
 import matplotlib.pyplot as plt
+import matplotlib.ticker
+import numpy as np
 import pandas as pd
 
 df = pd.read_csv("data.csv")
 
-fig = plt.figure()
+# Wall time.
+fig = plt.figure(1)
 ax = fig.gca()
-ax.set_yscale("log")
-ax.set_ylabel("wall time [seconds]")
 
-ax.plot(range(df.shape[0]), df["mean_9"]/df["mean_9"].iat[0])
-ax.plot(range(df.shape[0]), df["mean_8"]/df["mean_8"].iat[0])
-ax.plot(range(df.shape[0]), df["mean_7"]/df["mean_7"].iat[0])
-ax.plot(range(df.shape[0]), df["mean_6"]/df["mean_6"].iat[0])
+ax.errorbar(
+    df["no_cores"],
+    df["mean"],
+    yerr = df["mean"] * df ["stddev"] * 0.03,
+    fmt = "x",
+    capsize = 5,
+)
+ax.plot(
+    np.linspace(0.8, 20),
+    df["mean"].iat[0] / np.linspace(0.8, 20),
+    color = "k",
+    linestyle = "dashed",
+)
+
+ax.set_xlabel("number of goroutines")
+ax.set_ylabel("wall time [seconds]")
+ax.set_xscale("log")
+ax.set_yscale("log")
+ax.set_xlim(0.8, 20)
+ax.set_ylim(1, 25)
+ax.xaxis.set_major_formatter(matplotlib.ticker.FormatStrFormatter("%.0f"))
+ax.yaxis.set_major_formatter(matplotlib.ticker.ScalarFormatter())
 
 fig.tight_layout()
 fig.savefig("main.svg")
+
+# Number of cores times wall time.
+fig = plt.figure(2)
+ax = fig.gca()
+
+ax.errorbar(
+    df["no_cores"],
+    df["no_cores"] * df["mean"],
+    yerr = df["no_cores"] * df["mean"] * df ["stddev"] * 0.03,
+    fmt = "x",
+    capsize = 5,
+)
+ax.plot(
+    np.linspace(0, 17),
+    df["mean"].iat[0] * np.ones_like(np.linspace(0, 17)),
+    color = "k",
+    linestyle = "dashed",
+)
+
+ax.set_xlabel("number of goroutines")
+ax.set_ylabel("number of goroutines x wall time [seconds]")
+ax.set_xlim(0, 17)
+ax.set_ylim(0, 60)
+ax.xaxis.set_major_formatter(matplotlib.ticker.FormatStrFormatter("%.0f"))
+ax.yaxis.set_major_formatter(matplotlib.ticker.ScalarFormatter())
+
+fig.tight_layout()
+fig.savefig("main_1.svg")
